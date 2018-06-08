b8a43828557f6fdbc60eab69f927c398eb71c17c
Phung Xuan Anh <anhpx@sigma-solutions.eu>
2018-06-08 18:15:30 +0700

    Add view for front end

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import ProductList from '@/components/ProductList'
+import ProductCreate from '@/components/ProductCreate'
+import Home from '@/components/Home'
 
 Vue.use(Router)
 
@@ -10,6 +13,21 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/product-create',
+      name: 'ProductCreate',
+      component: ProductCreate
+    },
+    {
+      path: '/product-update/:pk',
+      name: 'ProductUpdate',
+      component: ProductCreate
     }
   ]
 })
