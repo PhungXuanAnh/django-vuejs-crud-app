189de3fe6f4663815b9d409bb4a4d64ea7fbf4c3
Phung Xuan Anh <anhpx@sigma-solutions.eu>
2018-06-08 16:17:10 +0700

    Add view for backend

---
+++ README.md
@@ -0,0 +1,9 @@
+# Usage
+
+```shell
+cd django-vuejs
+python3 -m virtualenv .python3
+source .python3/bin/activate
+pip install -r backend/requirement.txt
+```
+
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
+    'rest_framework',
     'catalog',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -41,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:8080',
+)
