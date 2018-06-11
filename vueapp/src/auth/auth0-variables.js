bc79f2b44a7c1dabef07d428a23ada9ba57589fe
Phung Xuan Anh <anhpx@sigma-solutions.eu>
2018-06-11 17:58:18 +0700

    Remove unsed code

--- vueapp/src/auth/auth0-variables.js
+++
@@ -1,6 +0,0 @@
-export const AUTH_CONFIG = {
-  clientId: 'TcHzYEyEzpJ0gYtYVyavXXjidRjQ7Yqw',
-  domain: 'techiediaries.auth0.com',
-  callbackUrl: 'http://localhost:8000/callback',
-  apiUrl: 'https://djangovuedemo.techiediaries.com'
-}
