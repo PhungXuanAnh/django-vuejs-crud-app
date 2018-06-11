4ebc2a2374efd98a591220c60476e01074ea526c
Phung Xuan Anh <anhpx@sigma-solutions.eu>
2018-06-11 17:21:44 +0700

    Remove auth service in app.vue

--- vueapp/src/App.vue
+++ vueapp/src/App.vue
@@ -32,28 +32,12 @@
 
 <script>
 /* eslint-disable */
-
-import AuthService from './auth/AuthService'
-
-const auth = new AuthService()
-
-const { login, logout, authenticated, authNotifier } = auth
-
 export default {
   name: 'app',
   data () {
-    authNotifier.on('authChange', authState => {
-      this.authenticated = authState.authenticated
-    })
-    return {
-      auth,
-      authenticated
-    }
+    return {}
   },
-  methods: {
-    login,
-    logout
-  }
+  methods: {}
 }
 </script>
 
