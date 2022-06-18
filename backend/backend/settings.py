c92f53c86f91d0cf41be629580d2c4259325805b
Phung Xuan Anh <phungxuananh1991@gmail.com>
2022-06-18 11:44:56 +0700

    update code

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -130,10 +130,10 @@ STATICFILES_DIRS = (
 STATIC_ROOT = os.path.join(BASE_DIR, 'public')
 
 CORS_ORIGIN_WHITELIST = (
-    'localhost:8080',
-    '127.0.0.1:8080',
-    'localhost:8000',
-    '127.0.0.1:8000',
+    'http://localhost:8080',
+    'http://127.0.0.1:8080',
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
 )
 
 WEBPACK_LOADER = {
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
-from django.conf.urls import url
+from django.urls import re_path as url
 
 from catalog import views
 
